629758aa8e5f9ee04007ac3d7f17d408f0c627f0
Juan Silva <darkjuan193@gmail.com>
2017-01-21 12:54:55 -0430

    Netflix
    Estoy creando un portal parecido al de netflix para una materia llamada
    metodologia de la investigacion. Este portal trabaja con php,
    javascript, html, css. Luego lo unire con un codigo de un algoritmo de
    recomendaciones que extrae datos de facebook para recomendaciones mas
    eficientes.

---
+++ Index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="es">
+	<?php include("head.php"); ?>
+
+<body>
+	<?php include("header.php"); ?>
+	
+	<section class="contenido">
+		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec sollicitudin dui volutpat ante cursus, sed mattis dolor convallis. Vestibulum accumsan euismod tincidunt. Mauris et nisl iaculis, ullamcorper enim ut, ultricies est. Fusce dignissim sem quis commodo dictum. Maecenas a augue quis diam scelerisque porttitor. Suspendisse vitae consequat sem, sed rhoncus elit. Maecenas gravida quam vitae odio lacinia faucibus. Pellentesque a vulputate tortor. Suspendisse non lectus libero. Mauris ultrices orci eu nisi posuere, sed mollis arcu condimentum. Proin at interdum augue, eu tristique nibh. Cras ut augue nec nisl fermentum vehicula eget vitae ex. Maecenas molestie sodales justo, id scelerisque tortor consectetur ac. Maecenas ac luctus nibh. Suspendisse porta elit at ante vehicula volutpat eu a ante.
+		</p>
+		<p>Vivamus facilisis ipsum eu ligula gravida condimentum. Cras suscipit ligula sed turpis condimentum pulvinar. Morbi auctor scelerisque ex, a cursus arcu tincidunt nec. Vestibulum imperdiet laoreet venenatis. Maecenas luctus at nisl a dapibus. Vivamus eget vehicula velit. Vivamus aliquam eros purus, nec fermentum lectus malesuada eu. Duis aliquam nunc elit. Praesent ac nulla pharetra, vestibulum arcu vulputate, accumsan ante.
+		</p>
+		<p>Curabitur hendrerit eros eros, non fringilla sem rutrum vitae. Sed turpis ex, laoreet vitae lobortis eu, gravida sit amet ipsum. Phasellus neque ligula, fermentum in ligula in, suscipit varius justo. Vivamus sollicitudin euismod sem at varius. Curabitur ultrices ac massa vestibulum tincidunt. Suspendisse a varius eros. Nulla facilisi. Nullam pretium vestibulum mi, eget varius dolor faucibus ut. Quisque mollis ullamcorper ipsum, lobortis porttitor ipsum pellentesque eu. Curabitur a tempus ligula. Donec vitae malesuada dui, quis vulputate massa. Nunc nec placerat ex, vitae sagittis tortor. Nam sem eros, consectetur ut eros non, gravida ultricies erat.
+		</p>
+		<p>Integer in suscipit felis. Sed interdum magna odio, nec accumsan lectus ultricies ac. Curabitur dignissim quam est, eu fermentum arcu pellentesque id. Nulla facilisi. Phasellus pulvinar tempus augue ut porta. Aliquam quis luctus dolor. Etiam egestas nunc sit amet nibh ullamcorper commodo vitae sit amet velit. Mauris vitae massa lacus. Nam ut tellus at purus aliquam convallis id quis neque. Donec placerat, urna at ultricies pretium, nunc justo condimentum ex, id iaculis eros justo vitae neque. Cras bibendum, sapien venenatis fermentum viverra, justo nunc consequat neque, sed pharetra turpis nunc at tortor. Sed consequat sed augue vel euismod. Proin diam risus, volutpat in vestibulum at, vulputate non risus. Curabitur tellus sem, aliquam quis condimentum condimentum, faucibus sit amet ligula. Nulla facilisi.
+		</p>
+	</section>
+
+	<?php include("footer.php"); ?>
+
+	<script src="js/jquery.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+
+</body>
+
+</html>
---
+++ header.php
@@ -0,0 +1,14 @@
+<header>
+	<div class="logo">
+		<img src="imagenes\Netflix3.png" width="300" height="170" class="img-responsive img-rounded">
+	</div>
+
+	<nav>
+		<a href="#">Inicio</a>
+		<a href="#">Servicios</a>
+		<a href="#">Contacto</a>
+		<a href="#">Login</a>
+		<a href="#">Registro</a>
+	</nav>
+	
+</header>
